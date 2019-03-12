e54110bb3c6fea3de50a1d597d92c8a201ded86c
Sasa Vukanovic <sasa.vukanovic@t-mobile.cz>
2019-03-13 00:15:08 +0100

    Solve views for Login, Signup - problems with routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  post 'signup', to: 'users#new',as: 'create_user'
   #get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
